4bf1a4caa49782583340fd4061eea8d120c4aa5b
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2017-12-28 20:03:17 +0800

    Ensure that android root directort exists

--- hooks/android.js
+++ hooks/android.js
@@ -5,7 +5,11 @@ module.exports = function (context) {
   const cordovaDirectory = path.resolve(context.opts.projectRoot);
   const googleServicesSource1 = path.resolve(cordovaDirectory, './google-services.json');
   const googleServicesSource2 = path.resolve(cordovaDirectory, '../', './google-services.json');
-  const googleServicesTarget = path.resolve(cordovaDirectory, './platforms/android/google-services.json');
+  const googleServicesTargetDirectory = path.resolve(cordovaDirectory, './platforms/android');
+  const googleServicesTarget = path.resolve(googleServicesTargetDirectory, './platforms/android/google-services.json');
+
+  // ensure root directory exists
+  filesystem.ensureDirSync(googleServicesTargetDirectory)
 
   // find and copy google services to root directory
   if (filesystem.existsSync(googleServicesSource1)) {
